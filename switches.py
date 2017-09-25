f170ad9ef99826b52c33b81f1b598c217897ffa4
Steve Holden <sh@felix.com>
2017-09-25 22:32:21 +0100

    Generalise to handle multiple switches. Simplify sense inversion.

--- switches.py
+++ switches.py
@@ -1,19 +1,56 @@
-# Simple switch debouncer
+"""Simple switch debouncer"""
 from machine import Timer, Pin
-#import utime, time
 
-class Debounced:
-    def __init__(self, pin):
-        self.pin = pin
-        self._state = 0
-        self.value = False
+class Debouncer:
+
+    def __init__(self):
+        "Initialise debounce"
+        self.switches = []
         self.timer = Timer(-1)
-        self.timer.init(period=8, mode=Timer.PERIODIC, callback=self.tick)
+        self.timer.init(period=6, mode=Timer.PERIODIC, callback=self.tick)
+
+    def register(self, switch):
+        "Add a switch to the bank."
+        self.switches.append(switch)
+        switch.pin.init(mode=Pin.IN, pull=Pin.PULL_UP)
+        return switch
+
     def tick(self, _):
-        bit = self.pin.value()
-        self._state = ((self._state << 1) | (1 - bit)) & 0xfff
-        if self._state == 0xfff:
-            self.value = True
-        elif self._state == 0x000:
-            self.value = False
-        # no action otherwise
+        "Examine input states and note debounced state changes."
+        for switch in self.switches:
+            bit = switch.pin.value()
+            switch._state = ((switch._state << 1) | bit) & 0xfff
+            if switch._state == 0xfff:     # switch opened
+                switch.value = False
+            elif switch._state == 0x000:   # switch closed
+                switch.value = True
+            # print(int(switch.value), end="")
+
+class Switch:
+
+    def __init__(self, pin_no, name):
+        self._state = 0
+        self.value = 0
+        self.pin = Pin(pin_no)
+        self.name = nygame
+
+    def output(self):
+        return self.name if self.value else "."
+
+d = Debouncer()
+
+Y = d.register(Switch(0, "Y")) # D3
+R = d.register(Switch(14, "R")) # D5
+W = d.register(Switch(12, "W")) # D6
+B = d.register(Switch(13, "B")) # D7
+
+def switches():
+    return "".join(x.output() for x in (Y, R, W, B))
+
+state = switches()
+
+while True:
+    new_state = switches()
+    if new_state != state:
+        state = new_state
+        print(state)
